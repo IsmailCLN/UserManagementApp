3b18de3146ef3f473816004d47ea51da676df600
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2023-06-03 16:15:26 +0300

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using UserManagementApp.Models;
+
+namespace UserManagementApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        [HttpGet]
+        
+        void connectionString()
+        {
+            con.ConnectionString = "data source"
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
